cee103e9e3f68a716eaccef55f29832b963f54d6
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-24 22:16:42 +0700

    Update 44.2n+1 chia 2n+2.py

--- 44.2n+1 chia 2n+2.py
+++ 44.2n+1 chia 2n+2.py
@@ -8,6 +8,6 @@ Created on Tue Sep 24 21:20:23 2024
 
 n = int(input("Nhập n: "))
 tong = 0
-for i in range(1, n+1):
+for i in range(n+1):
     tong += (2*i + 1) / (2*i + 2)
 print("Tổng S(n) =", tong)
