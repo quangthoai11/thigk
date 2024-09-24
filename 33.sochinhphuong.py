3b69f5857a609a3c1962d59369eef6f50700fd32
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-24 23:19:47 +0700

    Update 33.sochinhphuong.py

--- 33.sochinhphuong.py
+++ 33.sochinhphuong.py
@@ -6,14 +6,9 @@ Created on Sun Sep 22 12:09:59 2024
 """
 import math
 
-# Nhập số nguyên dương n
-n = int(input("Nhập số nguyên dương n: "))
-
-# Tính căn bậc hai của n và làm tròn xuống thành số nguyên
-sqrt_n = int(math.sqrt(n))
-
-# Kiểm tra xem bình phương của căn bậc hai có bằng n không
-if sqrt_n * sqrt_n == n:
-    print(n, "là số chính phương")
+import math
+n = int(input("Nhập vào số nguyên dương n: "))
+if int(math.sqrt(n)) ** 2 == n:
+    print(f"{n} là số chính phương")
 else:
-    print(n, "không phải là số chính phương")
+    print(f"{n} không phải là số chính phương")
