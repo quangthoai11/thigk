7638a51f2fe2f3b2acb831326b786512f80a3893
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-24 22:59:34 +0700

    Update 31.tamgiac.py

--- 31.tamgiac.py
+++ 31.tamgiac.py
@@ -5,25 +5,23 @@ Created on Sat Sep 21 22:01:42 2024
 @author: Admin
 """
 
-a = int(input("Nhập cạnh a: "))
-b = int(input("Nhập cạnh b: "))
-c = int(input("Nhập cạnh c: "))
-
-# Kiểm tra điều kiện tạo thành tam giác
-if a + b > c and b + c > a and c + a > b:
-    # Là tam giác
+a = int(input("Nhập số nguyên dương a: "))
+b = int(input("Nhập số nguyên dương b: "))
+c = int(input("Nhập số nguyên dương c: "))
+if a + b > c and a + c > b and b + c > a:
     if a == b == c:
-        print("Tam giác đều")
-    elif a == b or b == c or c == a:
-        print("Tam giác cân")
-    elif a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or c**2 + a**2 == b**2:
-        print("Tam giác vuông")
+        print("Tam giác đều.")
+    elif a == b or b == c or a == c:
+        if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+            print("Tam giác vuông cân.")
+        else:
+            print("Tam giác cân.")
+    elif a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+        print("Tam giác vuông.")
     else:
-        print("Tam giác thường")
+        print("Tam giác thường.")
 else:
-    print("Ba số đã nhập không tạo thành một tam giác")
-    
-    
+    print("Không phải là tam giác.")
     
     
     
