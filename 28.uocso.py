3a6de733750f4a99c92fd60ce256a0886fdfbb02
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-24 23:11:22 +0700

    Update 28.uocso.py

--- 28.uocso.py
+++ 28.uocso.py
@@ -8,7 +8,6 @@ Created on Sat Sep 21 21:43:54 2024
 N = int(input("Nhập vào một số nguyên dương N: "))
 while N < 0:
       N = int(input("Nhập lại một số nguyên dương N: "))
-print(f"Các ước số của {N} là:")
 for i in range(1, N + 1):
     if N % i == 0:
         print(i)
