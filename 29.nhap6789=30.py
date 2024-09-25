b93b6b1c73706f20ec4ceecd44792a0197832d69
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-25 10:07:31 +0700

    Update 29.nhap6789=30.py

--- 29.nhap6789=30.py
+++ 29.nhap6789=30.py
@@ -7,6 +7,8 @@ Created on Sat Sep 21 21:52:17 2024
 
 N = int(input("Nhập vào một số nguyên dương N: "))
 tong = 0
+while N <= 0:
+    N = int(input("N phải là số nguyên dương. Nhập lại N: "))
 while N > 0:
     tong += N % 10
     N //= 10
