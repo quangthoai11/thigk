1cd353229cd4a82d0b96870c916499c0f0586f17
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-25 10:08:31 +0700

    Update 34.songuyento.py

--- 34.songuyento.py
+++ 34.songuyento.py
@@ -6,16 +6,14 @@ Created on Sun Sep 22 12:13:44 2024
 """
 
 n = int(input("Nhập vào số nguyên dương n: "))
-if n <= 1:
-    print(f"{n} không phải là số nguyên tố.")
-else:
+while n <= 0:
+    n = int(input("n phải là số nguyên dương. Nhập lại n: "))
+if n > 1:
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             print(f"{n} không phải là số nguyên tố.")
             break
     else:
         print(f"{n} là số nguyên tố.")
-        
-        
-        
-        
+else:
+    print(f"{n} không phải là số nguyên tố.")
